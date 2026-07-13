22edc27c7ab7ec160755ad47f114525bae43a805
thedragon44-superscale <flightg@thedragonhms.com>
2026-07-13 09:09:16 -0400

    Integrated PWA shell, Service Worker, and deferred install trigger

---
+++ static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'streetbook-v1';
+const ASSETS_TO_CACHE = [
+    '/static/streetbook_logo.png',
+    '/static/sb.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
